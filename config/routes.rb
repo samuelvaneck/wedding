a9845eb78c983ae037381d53da9982badcf2c2ce
Samuel van Eck <samuel@simbeyond.com>
2018-09-10 19:55:32 +0200

    Create admin namespace + admin families controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :family do
-    resources :guests
-    resources :messages
+
+  namespace :admin do
+    resources :families do
+      resources :guest
+      resources :messages
+    end
+    resources :photos
+  end
+
+  resources :families, only: [:show, :update] do
+    resources :guests, only: [:show, :update]
+    resources :messages, only: [:create, :show, :update]
   end
   resources :photos
 end
