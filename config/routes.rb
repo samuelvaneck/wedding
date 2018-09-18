30a8be320e5e7249bdafc139b86bd9ffbce8a110
Samuel van Eck <samuel@simbeyond.com>
2018-09-18 20:31:57 +0200

    Removed photo model

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,11 @@ Rails.application.routes.draw do
     resources :guests
     resources :messages
     resources :posts
-    resources :photos
   end
 
   resources :families, only: [:show, :update] do
     resources :guests, only: [:show, :update]
     resources :messages, only: [:create, :show, :update]
   end
-  resources :photos
   resources :posts
 end
