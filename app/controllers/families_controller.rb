6c183f9b92d8f544d55a025ff745bcbb78afbca3
Samuel van Eck <samuel@simbeyond.com>
2018-09-30 19:08:28 +0200

    Added functionaly to submit the form

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,21 +1,24 @@
 class FamiliesController < ApplicationController
-  before_action :set_family, only: [:index, :update]
-  
+  before_action :set_family, only: [:update]
+
   def index
+    @family = Family.find_by(email: params[:email])
     @guests = @family.guests
+    @message = @family.message || Message.new
   end
 
   def update
+    @family.response = true
     @family.update(family_params)
   end
 
   private
 
   def family_params
-    params.require(:family).permit :response, :photo, guest_attributes: [:attending], message_attributes: [:content]
+    params.require(:family).permit :response, :photo, guests_attributes: [:id, :attending], message_attributes: [:content]
   end
 
   def set_family
-    @family = Family.find_by(email: params[:email])
+    @family = Family.find params[:id]
   end
 end
