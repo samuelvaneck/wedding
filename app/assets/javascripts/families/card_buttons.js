1c675b57a0873d9f858ffa3c3905d43dfad1310e
Samuel van Eck <samuel@simbeyond.com>
2018-12-01 17:18:35 +0100

    Changed RSVP card to wedding code as default

--- app/assets/javascripts/families/card_buttons.js
+++ app/assets/javascripts/families/card_buttons.js
@@ -1,4 +1,4 @@
-$(document).on("click", "#card-info-btn, #card-rsvp-btn, #card-post-btn", (event) => {
+$(document).on("click", "#card-info-btn, #card-uuid-btn, #card-post-btn", (event) => {
   let cardId = $(event.target).attr("id").replace("-btn", "");  
   $.get("/families/flip_card", { card_id: cardId }, null, "script");
 });
