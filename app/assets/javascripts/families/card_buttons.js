94f8ad3addfdbce7481dfb7d11bd3af33bf42865
Samuel van Eck <samuel@simbeyond.com>
2018-12-01 19:14:24 +0100

    Every action is done with card flip. Deleted unused code

--- app/assets/javascripts/families/card_buttons.js
+++ app/assets/javascripts/families/card_buttons.js
@@ -1,5 +1,6 @@
-$(document).on("click", "#card-info-btn, #card-uuid-btn, #card-post-btn", (event) => {
-  let cardId = $(event.target).attr("id").replace("-btn", "");  
-  $.get("/families/flip_card", { card_id: cardId }, null, "script");
+$(document).on("click", "#card-info-btn, #card-reply-btn, #card-post-btn", (event) => {
+  const cardId = $(event.target).attr("id").replace("-btn", "");
+  const uuid = localStorage.getItem("uuid");
+  $.get("/families/flip_card", { card_id: cardId, uuid: uuid }, null, "script");
 });
 
--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,27 +1,33 @@
+# frozen_string_literal: true
+
 class FamiliesController < ApplicationController
   before_action :set_family, only: [:update]
 
-  def index
-    return unless params[:uuid]
-    @family = Family.find_by(uuid: params[:uuid])
-    return unless @family
-    @guests = @family.guests
-    @message = @family.message || Message.new
-  end
+  def index; end
 
   def update
     @family.response = true
-    @success = @family.update(family_params)
+    success = @family.update(family_params)
+    @card_id = success ? "card-success" : "card-error"
+    @guests = @family.guests
+    @message = @family.message || Message.new
+    render 'flip_card'
   end
 
   def flip_card
     @card_id = params[:card_id]
+    if params[:uuid]
+      @family = Family.find_by(uuid: params[:uuid])
+      return unless @family
+      @guests = @family.guests
+      @message = @family.message || Message.new
+    end
   end
 
   private
 
   def family_params
-    params.require(:family).permit(:response, :photo, guests_attributes: [:id, :attending], message_attributes: [:content, :id])
+    params.require(:family).permit(:response, :photo, guests_attributes: %i[id attending], message_attributes: %i[content id])
   end
 
   def set_family
