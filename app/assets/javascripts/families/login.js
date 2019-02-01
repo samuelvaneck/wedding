3742d0d61fb6d96be3efef3a48b20faea681326a
Samuel van Eck <samuel@simbeyond.com>
2019-02-01 08:31:30 +0100

    Fixed issue with deleting character and moving to next input. Now takes steps

--- app/assets/javascripts/families/login.js
+++ app/assets/javascripts/families/login.js
@@ -1,41 +1,52 @@
-$(document).ready(function() {
+var codeInput0 = false;
+var codeInput1 = false;
+var codeInput2 = false;
+var codeInput3 = false;
+var codeInput4 = false;
+var codeInput5 = false;
+
+$(document).ready(function () {
   $("#code-input-0").focus();
 });
 
-$(document).on("keyup", ".code-input-field", function(e) {
-  focusInputField($(this), e);
+$(document).on("keyup", ".code-input-field", (event) => {
+  const idx = $(event.target).attr("id").split("-")[2];
+  
+  focusInputField(event, idx);
   if (filledLoginCode()) { fetchFamily() };
+  window["codeInput" + idx] = $(event.target).val().length > 0;
 });
 
-function focusInputField(_this, e) {
-  const idx = $(_this).attr("id").split("-")[2];
-  if (e.keyCode == 8) {
-    if (idx == 0) { return };
-    $(_this).parent().prev("div").find("input").focus();
+function focusInputField(event, idx) { 
+  if (event.keyCode == 8) {
+    if (idx == 0) return;
+    if (window["codeInput" + idx]) return;
+    $(event.target).parent().prev("div").find("input").focus();
   } else {
     if (idx == 5) { return };
-    $(_this).parent().next("div").find("input").focus();
+    $(event.target).parent().next("div").find("input").focus();
   }
 }
 
 function filledLoginCode() {
   let filled = 0
-  $(".code-input-field").each(function(i, d) {
+  
+  $(".code-input-field").each((i, d) => {
     if ($(d).val().length > 0) { filled++ };
   });
   return filled == 6
 }
 
 function fetchFamily() {
-  let data     = {};
-  data.uuid    = inputUuid();
+  let data = {};
+  data.uuid = inputUuid();
   data.card_id = "card-reply";
 
   showSpinner();
   localStorage.setItem("uuid", inputUuid());
 
-  setTimeout(function() {
-    $.get("/families/flip_card", data, null, "script").fail(function() {
+  setTimeout(function () {
+    $.get("/families/flip_card", data, null, "script").fail(function () {
       hideSpinner();
       $("#status").find(".status-text").text("Geen gasten gevonden")
     });
@@ -44,7 +55,7 @@ function fetchFamily() {
 
 function inputUuid() {
   let uuid = "";
-  $(".code-input-field").map(function(i, d) {
+  $(".code-input-field").map(function (i, d) {
     uuid += $(d).val();
   });
   return uuid.toLowerCase();
