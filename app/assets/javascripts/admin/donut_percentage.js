33abb92a7ca5a71b5fbf00aef9ff255596c5f0f6
Samuel van Eck <samuel@simbeyond.com>
2019-02-12 19:59:25 +0100

    Added simulation dashboard

---
+++ app/assets/javascripts/admin/donut_percentage.js
@@ -0,0 +1,56 @@
+function percentageChart(id, percentage) {
+  const duration = 1500;
+  const transition = 200;
+  const width = 170;
+  const height = 170;
+
+  const dataset = {
+          lower: calcPercent(0),
+          upper: calcPercent(percentage) };
+  const radius = Math.min(width, height) / 3;
+  let pie = d3.pie().sort(null);
+  let format = d3.format(".0%"); 
+  const color = d3.scaleOrdinal(["#00539C", "#EEEEEE"])
+  const arc = d3.arc()
+                .innerRadius(radius * .8)
+                .outerRadius(radius);
+  let svg = d3.select(id).append("svg")
+              .attr("width", width)
+              .attr("height", height)
+              .append("g")
+              .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+  let path = svg.selectAll("path")
+                .data(pie(dataset.lower))
+                .enter().append("path")
+                .attr("fill", (d, i) => { return color(i); })
+                .attr("d", arc)
+                .each((d) => {
+                  this._current = d;
+                });
+
+  let text = svg.append("text")
+                .attr("text-anchor", "middle")
+                .attr("dy", ".3em");
+                
+  let progress = 0;
+  let timeout = setTimeout(function() {
+    clearTimeout(timeout);
+    path = path.data(pie(dataset.upper));
+    
+    path.transition().duration(duration).attrTween("d", function(a) {
+      const i = d3.interpolate(this._current, a);
+      const j = d3.interpolate(progress, percentage);
+      
+      this._current = i(0);
+      return function(t) {
+        text.text(format(j(t) / 100));
+        return arc(i(t));
+      }
+    });
+  }, 200);
+
+
+  function calcPercent(percentage) {
+    return [parseInt(percentage), 100 - parseInt(percentage)]
+  }
+}
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,8 @@ class Guest < ApplicationRecord
   belongs_to :family
 
   validates :name, presence: true
+
+  scope :is_attending, -> { where(attending: true) }
+  scope :not_attending, -> { where(attending: false).or(Guest.where(attending: nil)) 
+                           }
 end
