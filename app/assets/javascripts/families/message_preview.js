0c6d2cb7e5c3c8a04641d9d92ec106f917fa6201
Samuel van Eck <samuel@simbeyond.com>
2019-03-14 16:32:39 +0100

    Code refactoring and allergies preview

--- app/assets/javascripts/families/message_preview.js
+++ app/assets/javascripts/families/message_preview.js
@@ -1,5 +1,17 @@
-$(document).on("keyup", "#family_message_attributes_content", displayMessagePreview)
+$(document).on("keyup", "#family_message_attributes_content", displayPreview);
+$(document).on("keyup", "#family_allergies", displayPreview);
 
-function displayMessagePreview(event) {
-  $("#message-preview").text($(event.target).val());
+
+function displayPreview(event) {
+  const _this = $(event.target);
+  const messagePreview = _this.attr("id") == "family_allergies"
+  let previewText = "";
+
+  if (_this.val().length == 0) {
+    previewText = messagePreview ? "Allergiën voorbeeld" : "Bericht voorbeeld";
+  } else {
+    previewText = _this.val();
+  }
+
+  _this.closest(".d-flex").find(".preview").text(previewText);
 }
