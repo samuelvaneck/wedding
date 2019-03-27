3ae122c3a4e0c39eb032d8b9788177bbf2458a92
Samuel van Eck <samuel@simbeyond.com>
2019-03-27 07:43:28 +0100

    Improved sorting of guests

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,8 +3,8 @@ class Admin::DashboardsController < AdminController
   def index
     @responded = Family.responded
     @awaiting_respone = Family.not_responded
-    @guests_present = Guest.is_attending
-    @guests_absent = Guest.not_attending
+    @family_guests_present = Family.joins(:guests).where("guests.attending = ?", true).uniq
+    @family_guests_absent = Family.joins(:guests).where("guests.attending is null OR guests.attending = ?", false).uniq
     @messages = Message.all
   end
 end
--- app/helpers/families_helper.rb
+++ app/helpers/families_helper.rb
@@ -11,4 +11,12 @@ module FamiliesHelper
     attending = family.guests.map { |guest| true if guest.attending.present? }.compact.count
     return "#{attending} / #{family.guests.count}"
   end
+
+  def response_badge(family)
+    badge_class = family.response ? "badge badge-success" : "badge badge-secondary"
+    badge_text = family.response ? "Responded" : "Awaiting response"
+    tag.span class: badge_class do
+      badge_text
+    end
+  end
 end
