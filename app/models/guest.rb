ae7f154c409d301bf46e839d7c16582719a4ad0f
Samuel van Eck <samuel.van.eck@simbeyond.com>
2019-05-05 19:05:28 +0200

    Added total attending and absent day and evening guest to dashboard

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,5 +4,9 @@ class Guest < ApplicationRecord
   validates :name, presence: true
 
   scope :is_attending, -> { where(attending: true) }
+  scope :day_guests_attending, -> { where(day_guest: true, attending: true) }
+  scope :evening_guests_attending, -> { where(day_guest: false, attending: true) }
+  scope :day_guests_not_attending, -> { where(day_guest: true, attending: false).or(Guest.where(day_guest: true, attending: nil)) }
+  scope :evening_guests_not_attending, -> { where(day_guest: false, attending: false).or(Guest.where(day_guest: false, attending: nil)) }
   scope :not_attending, -> { where(attending: false).or(Guest.where(attending: nil)) }
 end
