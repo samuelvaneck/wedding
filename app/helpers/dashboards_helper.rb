f4e3ce30f82785ddee57997a4587dfc688292a49
Samuel van Eck <samuel.van.eck@simbeyond.com>
2019-05-06 08:01:03 +0200

    Code refactoring

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,9 +1,5 @@
 module DashboardsHelper
   def percentage(part, total)
-    if total.zero?
-      0
-    else
-     part.to_f / total.to_f * 100
-    end
+    total.zero? ? 0 : part.to_f / total.to_f * 100
   end
 end
