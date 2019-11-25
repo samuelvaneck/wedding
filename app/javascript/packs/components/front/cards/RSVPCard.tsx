21434eef939ea4279ab5917f21686c306366986b
Samuel van Eck <samuel.van.eck@simbeyond.com>
2019-11-25 07:51:19 +0100

    Add ajax request for sending form rsvp

--- app/javascript/packs/components/front/cards/RSVPCard.tsx
+++ app/javascript/packs/components/front/cards/RSVPCard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { mount } from '../../../application/mount'
 import { InputType } from 'zlib'
+import { runInThisContext } from 'vm'
 
 interface Guest {
   id: number
@@ -15,6 +16,7 @@ interface Message {
 
 interface RSVPCardProps {
   family: {
+    id: number
     name: string
     guests: {
       [key: string]: Guest
@@ -24,7 +26,7 @@ interface RSVPCardProps {
 }
 
 interface RSVPCardState {
-  family: { name: string, guests: { [key: string]: Guest }, message: { content: string }}
+  family: { id: number, name: string, guests: { [key: string]: Guest }, message: { content: string }}
   submitSuccess: boolean
 }
 
@@ -32,39 +34,62 @@ const getInitialFamily = (props: RSVPCardProps) => props.family
 
 export class RSVPCard extends React.Component<RSVPCardProps, RSVPCardState> {
   state: RSVPCardState
-
+  
   constructor(props: RSVPCardProps) {
     super(props);
     this.state = {
       family: getInitialFamily(this.props),
       submitSuccess: false
     }
+
+    this.handleChangeGuestAttending = this.handleChangeGuestAttending.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChangeMessageContent() {
     console.log(event);
   }
 
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement
-    const data = new FormData(form);
+  handleChangeGuestAttending(event: React.ChangeEvent<HTMLInputElement>, idx: number) {
+    const target = event.target;
+    const value = target.checked;
+    let family = { ...this.state.family };
+    let guest = { ...this.state.family.guests[idx] }
+    guest.attending = value;
+    family.guests[idx] = guest
+    this.setState({ family })
+  }
 
-    fetch('/famlies/update', {
+  handleSubmit(event: React.FormEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const family = { ...this.state.family };
+    const elem: HTMLMetaElement = document.querySelector('[name="csrf-token"]');
+    const csrfToken = elem.content;
+
+    fetch('/families/' + family.id, {
       method: 'PUT',
-      body: data
+      body: JSON.stringify(this.state),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+        'X-Requested-With': 'XMLHTTPRequest' 
+      }
     }).then(response => response.json)
   }
 
   render() {
     const guestsInputFields = Object.keys(this.state.family.guests).map((key, idx) => {
       const guest = this.state.family.guests[key];
+      const attending = guest.attending ? 't' : 'f'
       return(
         <li className="list-group-item" key={idx}>
           <div className="d-flex">
             <div>{guest.name}</div>
             <div className="ml-auto mr-4">
-              <input type='checkbox' name='attending' />
+              <input type='checkbox' name='attending' 
+                     onChange={(event) => this.handleChangeGuestAttending(event, idx)}
+                     defaultValue={attending} />
             </div>
           </div>
         </li>
@@ -80,7 +105,7 @@ export class RSVPCard extends React.Component<RSVPCardProps, RSVPCardState> {
             </div>
           </div>
           <div className="card-body">
-            <form onSubmit={this.handleSubmit}>
+            <form>
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
                   <div className="d-flex">
@@ -97,7 +122,7 @@ export class RSVPCard extends React.Component<RSVPCardProps, RSVPCardState> {
                 </li>
                 <li className="list-group-item">
                   <div className="d-flex justify-content-end">
-                      <input type="submit" value='Verstuur' className='btn btn-outline-primary' />
+                      <input type="submit" value='Verstuur' className='btn btn-outline-primary' onClick={(event) => this.handleSubmit(event) } />
                   </div>
                 </li>
               </ul>
