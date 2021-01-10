f3aa0fa6367497ad498cdf7ed94228ae4944d690
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-01-10 15:24:59 +0100

    Add app url whitelist

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # whitlist host name
+  config.hosts << ENV['APP_URL']
 end
