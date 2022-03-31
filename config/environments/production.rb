332f2ff429f14ad0ef1d94a416aff98dd93f73b9
Samuel Van Eck <samuelvaneck@adcombi.com>
2022-03-31 13:43:19 +0200

    Remove app host enviroment url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # whitlist host name
-  config.hosts << ENV['APP_URL']
 end
